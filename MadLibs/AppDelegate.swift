1153a59d37cb5f5026d33187ae9cfbcfcaab8061
Colin Mullikin <ColinMullikin@colins-mbp.att.net>
2016-12-25 23:50:44 -0600

    Initial Commit

---
+++ MadLibs/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MadLibs
+//
+//  Created by Colin Mullikin on 12/25/16.
+//  Copyright © 2016 Colin Mullikin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
